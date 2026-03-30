ba51cbbcebb844f2ba42f7cc4466aa74f5ffeb98
des1re26 <des1re26@mail.ru>
2026-03-31 01:51:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,58 @@
-// Copyright 2022 NNTU-CS
-#include <cstdint>
-#include "alg.h"
+// Copyright 2024 <Student>
 
+#include "alg.h"
+#include <cmath>
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        sum += calcItem(x, n);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        double term = pown(x, 2 * n + 1) / fact(2 * n + 1);
+        if (n % 2 == 0) {
+            sum += term;
+        } else {
+            sum -= term;
+        }
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        double term = pown(x, 2 * n) / fact(2 * n);
+        if (n % 2 == 0) {
+            sum += term;
+        } else {
+            sum -= term;
+        }
+    }
+    return sum;
 }
